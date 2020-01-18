584c45b38f6f7d36bc05c1d70452ec36e4a29a89
Ramgopalbhat10 <subramanyaramgopal@gmail.com>
2020-01-18 11:29:28 +0530

    moved header to navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 function App() {
   return (
     <div className="App">
-      <h2>Welcome</h2>
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
