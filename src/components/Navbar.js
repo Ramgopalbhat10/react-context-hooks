50349b09474d040c9eba9eb13da7b0576b7b99e7
Ramgopalbhat10 <subramanyaramgopal@gmail.com>
2020-01-19 20:27:54 +0530

    composed multiple HOC components of contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { compose } from 'recompose';
 import withTheme from '../hoc/withTheme';
+import withAuth from '../hoc/withAuth';
 
 class Navbar extends Component {
   render() {
@@ -7,6 +9,9 @@ class Navbar extends Component {
     return (
       <nav style={{ background: ui, color: syntax }}>
         <h2>Welcome to Course Tracker</h2>
+        <div>
+          {this.props.authStatus ? "Logged in" : "Logged out"}
+        </div>
         <ul>
           <li>Home</li>
           <li>About</li>
@@ -17,4 +22,7 @@ class Navbar extends Component {
   }
 }
 
-export default withTheme(Navbar);
+export default compose(
+  withTheme,
+  withAuth
+)(Navbar);
